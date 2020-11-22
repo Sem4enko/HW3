07a77ab4f069ac6efc711e7ac9d6af30f9ae9d70
Sem4enko <74368102+Sem4enko@users.noreply.github.com>
2020-11-22 12:03:33 +0200

    Update Circle.java

--- src/homework3/Circle.java
+++ src/homework3/Circle.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 //Task1
 public class Circle {
     public static void main(String[] args) {
+             t();
+    }
+    static void t(){
         Scanner a = new Scanner(System.in);
         System.out.print("Radius:");
         double radius = a.nextDouble();
